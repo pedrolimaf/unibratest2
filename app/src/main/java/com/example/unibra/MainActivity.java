3590903aa6a15343648691864253224715db9ebd
Pedro Lima <pedro.vitor@mail.com>
2021-03-23 19:22:09 -0300

    fist commit

---
+++ app/src/main/java/com/example/unibra/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.unibra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tarefa tarefa = new Tarefa();
+                tarefa.execute("http://192.168.0.9/unibra/public/api/list");
+            }
+        });
+    }
+
+    private class Tarefa extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String retorno = conexao.getDados(strings[0]);
+            return retorno;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            textView.setText(s);
+        }
+    }
+
+}
